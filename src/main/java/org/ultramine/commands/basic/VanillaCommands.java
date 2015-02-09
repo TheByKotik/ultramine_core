6661385a6107b4d3fcd7dc169b4d70e66d116225
vlad20012 <beskvlad@yandex.ru>
2015-02-09 20:26:24 +0300

    Обновлена ванильная команда gamerule с использованием MultiWorld

--- src/main/java/org/ultramine/commands/basic/VanillaCommands.java
+++ src/main/java/org/ultramine/commands/basic/VanillaCommands.java
@@ -13,9 +13,11 @@ import net.minecraft.util.ChatStyle;
 import net.minecraft.util.StatCollector;
 import static net.minecraft.util.EnumChatFormatting.*;
 import net.minecraft.world.EnumDifficulty;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.storage.WorldInfo;
 
+import org.apache.commons.lang3.StringUtils;
 import org.ultramine.commands.Command;
 import org.ultramine.commands.CommandContext;
 import org.ultramine.commands.IExtendedCommand;
@@ -238,6 +240,48 @@ public class VanillaCommands
 		{
 			world.setWorldTime(curTime + time);
 			ctx.sendMessage("commands.time.added", time);
+		}
+	}
+	
+	@Command(
+			name = "gamerule",
+			group = "vanilla",
+			permissions = {"command.vanilla.difficulty"},
+			syntax = {
+					"",
+					"<key>",
+					"<world> <key>",
+					"<key> <value>",
+					"<world> <key> <value>"
+			}
+	)
+	public static void gamerule(CommandContext ctx)
+	{
+		GameRules rules = (ctx.contains("world") ? ctx.get("world").asWorld() : ctx.getSenderAsPlayer().getServerForPlayer()).getGameRules();
+		if(ctx.contains("value"))
+		{
+			String key = ctx.get("key").asString();
+			if(rules.hasRule(key))
+			{
+				rules.setOrCreateGameRule(key, ctx.get("value").asString());
+				ctx.sendMessage("commands.gamerule.success");
+			}
+			else
+			{
+				ctx.sendMessage("commands.gamerule.norule", key);
+			}
+		}
+		else if(ctx.contains("key"))
+		{
+			String key = ctx.get("key").asString();
+			if(rules.hasRule(key))
+				ctx.sendMessage("%s = %s", key, rules.getGameRuleStringValue(key));
+			else
+				ctx.sendMessage("commands.gamerule.norule", key);
+		}
+		else
+		{
+			ctx.sendMessage(StringUtils.join(rules.getRules(), ", "));
 		}
 	}
 }
