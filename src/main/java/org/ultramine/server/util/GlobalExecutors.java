1f2214e279dfdb51b677ca28453ad54f7c97a223
vlad20012 <beskvlad@yandex.ru>
2015-03-24 15:56:57 +0400

    Заданы названия потокам общих экзекьюторов

--- src/main/java/org/ultramine/server/util/GlobalExecutors.java
+++ src/main/java/org/ultramine/server/util/GlobalExecutors.java
@@ -3,10 +3,12 @@ package org.ultramine.server.util;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
 public class GlobalExecutors
 {
-	private static final ExecutorService io = Executors.newSingleThreadExecutor();
-	private static final ExecutorService cached = Executors.newCachedThreadPool();
+	private static final ExecutorService io = Executors.newSingleThreadExecutor(new ThreadFactoryBuilder().setNameFormat("UM IO #%d").setDaemon(true).build());
+	private static final ExecutorService cached = Executors.newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat("UM cached  #%d").setDaemon(true).build());
 
 	/**
 	 * Обрабатывает задачи на сохранение чего-либо на диск/в БД. Используется
