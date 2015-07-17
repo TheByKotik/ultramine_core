c401e4649a931b5679dd11d179d6e9026a5e6e24
vlad20012 <beskvlad@yandex.ru>
2015-07-17 22:08:49 +0300

    Добавлены методы для распаковки long key блока в x y z

--- src/main/java/org/ultramine/server/chunk/ChunkHash.java
+++ src/main/java/org/ultramine/server/chunk/ChunkHash.java
@@ -28,6 +28,27 @@ public class ChunkHash
 	
 	public static long blockCoordToHash(int x, int y, int z)
 	{
-		return (long)(x & 0xffffff) | ((long)(z & 0xffffff) << 32) | ((long)(y & 0xff) << 56);
+		return (long)(x & 0xffffff) | ((long)(y & 0xff) << 24) | ((long)(z & 0xffffff) << 32);
+	}
+	
+	public static int blockKeyToX(long key)
+	{
+		int x = (int)(key & 0xffffff);
+		if((x & 8388608) != 0)
+			x |= 0xff << 24;
+		return x;
+	}
+	
+	public static int blockKeyToZ(long key)
+	{
+		int z = (int)((key >> 32) & 0xffffff);
+		if((z & 8388608) != 0)
+			z |= 0xff << 24;
+		return z;
+	}
+	
+	public static int blockKeyToY(long key)
+	{
+		return (int)((key >> 24) & 0xff);
 	}
 }
